1d8c52c50b180862162edd49c89c482c800122c7
DavidLlanio <llaniodavid@gmail.com>
2024-07-21 22:46:30 -0400

    Skeleton code for database and generator classes

---
+++ helpers/database.py
@@ -0,0 +1,19 @@
+import os  # noqa: F401
+from collections import defaultdict
+
+
+class CharacterDatabase:
+    def __init__(self):
+        self.characters = defaultdict(dict)
+
+    def add_character():
+        pass
+
+    def add_buffer_event():
+        pass
+
+    def get_character_info():
+        pass
+
+    def get_character_leaderboard():
+        pass
---
+++ helpers/generator.py
@@ -0,0 +1,7 @@
+import os  # noqa: F401
+from random import choice, choices  # noqa: F401
+
+
+class Generator:
+    def __init__(self):
+        pass
