b35f9719dea8bbdc9d48e06aa18de92bf1b88664
DavidLlanio <llaniodavid@gmail.com>
2024-07-23 21:13:16 -0400

    Fully implemented simple generator and pre-commit

--- helpers/generator.py
+++ helpers/generator.py
@@ -1,7 +1,58 @@
-import os  # noqa: F401
-from random import choice, choices  # noqa: F401
+from event_generator import EventGenerator
+from item_generator import ItemGenerator
 
 
 class Generator:
-    def __init__(self):
-        pass
+    """
+    This class generates buffer items to be queued
+    """
+
+    def __init__(self, datapath):
+        """
+        The constructor for Generator class
+
+        Parameters:
+            data_path (os.Path): Path to data folder
+        """
+        # Composed of two other generators
+        self.item_generator = ItemGenerator(data_path=datapath)
+        self.event_generator = EventGenerator(data_path=datapath)
+
+    def generate_buffer_item(self, n_items):
+        """
+        This method generates n_items of events, creates items based
+        on winning events, then returns a dict with events and
+        items generated, and also how much exp.
+
+        Parameters:
+            n_items (int): Number of events to generate
+        Returns:
+            dict: buffer item with events, items, and exp
+        """
+        event_buffer = list(str)
+        item_buffer = list(str)
+        exp = 0
+
+        # Generate events
+        for _ in range(0, n_items):
+            event, points = self.event_generator.get_event()
+
+            if points:
+                exp += 1
+
+            event_buffer.append(event)
+
+        # For each winning event generate an item
+        for _ in range(0, exp):
+            item = self.item_generator.get_item()
+
+            item_buffer.append(item)
+
+        # Create dict representing buffer item
+        buffer_item = {
+            "events": event_buffer,
+            "items": item_buffer,
+            "exp": exp,
+        }
+
+        return buffer_item
