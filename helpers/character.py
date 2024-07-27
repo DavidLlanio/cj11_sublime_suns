78a3043d0bd278c4fe92cea506cf8e0290ee9cbc
DavidLlanio <llaniodavid@gmail.com>
2024-07-27 15:15:45 -0400

    Added name and equip best in slot function

--- helpers/character.py
+++ helpers/character.py
@@ -8,6 +8,7 @@ from item_generator import Item
 class Character:
     """Class for keeping track of character information"""
 
+    name: str = ""
     sex: str = ""
     race: str = ""
     class_: str = ""
@@ -21,3 +22,37 @@ class Character:
     buffer: list[BufferItem] = []
     quest_log: list[str] = []
     ranking_points: int = 0
+
+    def equip_best(self):
+        for item in self.inventory:
+            item_t = item.type_
+            if (
+                item_t == "Weapon"
+                and item.enumerate_rarity()
+                > self.equipped_weapon.enumerate_rarity()
+            ):
+                self.equipped_weapon = item
+            elif (
+                item_t == "Helmet"
+                and item.enumerate_rarity()
+                > self.equipped_helmet.enumerate_rarity()
+            ):
+                self.equipped_helmet = item
+            elif (
+                item_t == "Armor"
+                and item.enumerate_rarity()
+                > self.equipped_armor.enumerate_rarity()
+            ):
+                self.equipped_armor = item
+            elif (
+                item_t == "Boots"
+                and item.enumerate_rarity()
+                > self.equipped_boots.enumerate_rarity()
+            ):
+                self.equipped_boots = item
+            elif (
+                item_t == "Necklace"
+                and item.enumerate_rarity()
+                > self.equipped_necklace.enumerate_rarity()
+            ):
+                self.equipped_necklace = item
