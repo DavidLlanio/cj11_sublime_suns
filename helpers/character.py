c35f9774f9659edc2ae2e263acbdb1a8b95beade
DavidLlanio <llaniodavid@gmail.com>
2024-07-27 14:35:25 -0400

    Created character dataclass

---
+++ helpers/character.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+from generator import BufferItem
+from item_generator import Item
+
+
+@dataclass
+class Character:
+    """Class for keeping track of character information"""
+
+    sex: str = ""
+    race: str = ""
+    class_: str = ""
+    coins: int = 0
+    equipped_helmet: Item = None
+    equipped_armor: Item = None
+    equipped_boots: Item = None
+    equipped_necklace: Item = None
+    equipped_weapon: Item = None
+    inventory: list[Item] = []
+    buffer: list[BufferItem] = []
+    quest_log: list[str] = []
